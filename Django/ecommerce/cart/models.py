742317cbba673944ff37da09a8f70e51e95c91ac
Mustapha Osman <wnfynn@gmail.com>
2025-02-14 22:36:24 +0000

    dnone for the day

--- Django/ecommerce/cart/models.py
+++ Django/ecommerce/cart/models.py
@@ -6,3 +6,10 @@ class Product(models.Model):
     price = models.IntegerField()
     category = models.CharField(max_length = 200)
 
+
+    def __str__(self):
+        return f"id: {self.name} | description: {self.description} | price: {self.price} | category: {self.category}"
+
+""" 
+from cart.models import Product """
+""" product1 = Product(name = "iphone 14 pro", description = "The best mobilephone one the planet", price = 450, category = "Electonic Devices") """
