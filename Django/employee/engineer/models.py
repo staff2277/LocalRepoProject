ffcbf2376994d3d24016e63bfbaf052406edee79
Mustapha Osman <wnfynn@gmail.com>
2025-02-16 19:30:48 +0000

    working as admin

--- Django/employee/engineer/models.py
+++ Django/employee/engineer/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Engineers(models.Model):
+    name = models.CharField(max_length=205)
+    relegion = models.CharField(max_length=60)
+    age = models.IntegerField()
+    salary = models.IntegerField()
+
+    def __str__(self):
+        return f"name: {self.name} | relegion: {self.relegion} | age: {self.age} | salary: {self.salary}"
+    
+
