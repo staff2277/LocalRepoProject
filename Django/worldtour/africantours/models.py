953d99df0295616a6da399022da653e967620dba
Mustapha Osman <wnfynn@gmail.com>
2025-02-19 16:30:13 +0000

    migrated African tours

--- Django/worldtour/africantours/models.py
+++ Django/worldtour/africantours/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Afrotours(models.Model):
+    country = models.CharField(max_length=108)
+    tour_duration = models.IntegerField()
+    price = models.IntegerField()
+
+    def __str__(self):
+        return f"country => {self.name} \n tour_duration => {self.tour_duration} \n price => {self.price} \n"
