30f0ce06c964d5d34c34c4988c32aaf81fdc22fd
Mustapha Osman <wnfynn@gmail.com>
2025-02-21 18:08:30 +0000

    started africa tours project

--- Django/worldtour/africantours/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from .models import Afrotours
-
-# Create your views here.
-def index(request):
-    tours = Afrotours.objects.all()
-    context = {"tours": tours}
-    return render(request, "index.html", context)
--- Django/worldtour/africatours/apps.py
+++ Django/worldtour/africatours/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class EngineerConfig(AppConfig):
+class AfricatoursConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'engineer'
+    name = 'africatours'
