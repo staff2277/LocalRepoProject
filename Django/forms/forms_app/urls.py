634f8bb40c6b42d312a4661c40510b58f20c1886
Mustapha Osman <wnfynn@gmail.com>
2025-02-25 17:30:07 +0000

    building contact form

--- Django/forms/forms_app/urls.py
+++ Django/forms/forms_app/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index)
+    path("", views.home_page, name="home"),
+    path("contact/", views.contact_page, name="contact"),
+    path("contact_success/", views.contactSucess_page, name="contactSucess_page"),
 ]
--- Django/forms/forms_app/views.py
+++ Django/forms/forms_app/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import ContactForm
 
 # Create your views here.
 
-def index(request):
+def home_page(request):
     return render(request, "home.html")
+
+def contact_page(request):
+    if request.method == "POST":
+        form = ContactForm(request.post)
+        if form.is_valid:
+            form.sendEmail()
+            return redirect("contact_success")
+    else:
+        form = ContactForm()
+        context = {"form": form}
+        return render(request, "contact.html", context)
+    
+def contactSucess_page(request):
+    return render(request, "contact_success.html")
