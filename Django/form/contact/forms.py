7096a4c5c435c1525db2b7be6c66df8330a1d4af
Mustapha Osman <wnfynn@gmail.com>
2025-03-12 14:40:12 +0000

    created a new contact form

--- Django/form/contact/forms.py
+++ Django/form/contact/forms.py
@@ -3,7 +3,7 @@ from django import forms
 class contactForm(forms.Form):
     name = forms.CharField(max_length=100)
     email = forms.EmailField()
-    message = forms.Textarea()
+    message = forms.CharField(widget=forms.Textarea)
 
     def sendMessage(self):
         return f"\nName = {self.cleaned_data ["name"]} \n Email = {self.cleaned_data ["email"]} \n Message = {self.cleaned_data ["message"]} \n"
