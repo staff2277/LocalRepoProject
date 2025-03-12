1a8cf3bd96498f2881c8750f4405231a91d94040
Mustapha Osman <wnfynn@gmail.com>
2025-03-12 16:48:17 +0000

    contact succes page done

--- Django/contactForm/contact/urls.py
+++ Django/contactForm/contact/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("",views.home, name="home"),
-    path("contact",views.contactPage, name="contact")
+    path("contact/",views.contactPage, name="contact"),
+    path("contactSuccess/",views.contactSucess, name="contactSuccess"),
 ]
--- Django/contactForm/contact/views.py
+++ Django/contactForm/contact/views.py
@@ -1,9 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import ContactForm
 
 # Create your views here.
 def home(request):
     return render(request, "home.html" )
 
+def contactSucess(request):
+    return render(request, "contactSuccess.html" )
+
 
 def contactPage(request):
-    return render(request, "contact.html" )
+    form = ContactForm()
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+            return redirect("contactSuccess")
+        
+    context = {"form": form}
+    return render(request, "contact.html", context )
+
