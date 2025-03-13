17048e7c3bcef77c291be5cd33f9046f5776f405
Mustapha Osman <wnfynn@gmail.com>
2025-03-13 17:29:13 +0000

    q5

--- Django/contactForm/contact/views.py
+++ Django/contactForm/contact/views.py
@@ -3,6 +3,7 @@ from .forms import ContactForm
 
 # Create your views here.
 def home(request):
+    return render(request, "home.html" )
 
 def contactSucess(request):
     return render(request, "contactSuccess.html" )
